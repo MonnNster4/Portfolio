afea3cca8399c9d12a6dc17ac0c10c543bdb5c7f
MonnNster4 <monsantiago09@gmail.com>
2023-11-25 05:35:30 +0800

    main

--- src/assets/Navbar/Navbar.jsx
+++ src/assets/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+
 import { FaBarsStaggered } from "react-icons/fa6";
 
 function Navbar() {
@@ -7,14 +7,24 @@ function Navbar() {
 
   return (
     <>
-      <div className="w-full py-9">
+      <div className="py-7 px-8 flex justify-between shadow-md">
         <div>
-          <h1>Mon</h1>
+          <h1>Hello</h1>
         </div>
         <div>
           <button>
             <FaBarsStaggered />
           </button>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col p-16 gap-4" >
+          <h1 className="font-bold text-2xl">YNW Melly</h1>
+          <h5 className="">Rapper Artist</h5>
+          <p className="">Jamell Maurice Demons, known professionally as YNW Melly, is an American rapper and singer from Gifford, Florida. He rose to fame in 2018 following the commercial release of his single "Murder on My Mind", a trap song which explores homicidal ideation.</p>
+        </div>
+        <div>
+          
         </div>
       </div>
     </>
