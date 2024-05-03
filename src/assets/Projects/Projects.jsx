e8c17d8e067f1b770f6c5984cbde0b95b83adeb4
MonnNster4 <monsantiago09@gmail.com>
2024-05-03 22:07:36 +0800

    update

--- src/assets/Projects/Projects.jsx
+++ src/assets/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
-
 function Projects() {
   return (
     <>
@@ -12,7 +11,7 @@ function Projects() {
         <div className="flex flex-col gap-10 lg:gap-10 lg:items-center lg:grid lg:grid-cols-2 ">
           <div className="max-w-full shadow-lg shadow-black-500 h-80 relative bg-black opacity-90 rounded-t-2xl ">
             <img
-              src="./src/img/groupnb.jpg"
+              src={"./static/img/groupnb.JPG"}
               className="h-full w-full opacity-30"
             ></img>
             <div className="flex flex-col gap-4 absolute bottom-0 left-3 px-2 pb-4 z-50 text-white ">
@@ -21,15 +20,27 @@ function Projects() {
                 <p>A company website built with TailwindCSS and ReactJS.</p>
               </div>
               <div className="flex gap-3">
-                <a href="https://erp-immigration.groupnb.com.ph/ " target="_blank"><h4 className="hover:underline hover:cursor-pointer ">Visit Site</h4></a>
-                <a href="https://erp-immigration.groupnb.com.ph/ " target="_blank"><FaExternalLinkAlt className="mt-1 hover:scale-125 transition duration-200 hover:cursor-pointer" /></a>
+                <a
+                  href="https://erp-immigration.groupnb.com.ph/ "
+                  target="_blank"
+                >
+                  <h4 className="hover:underline hover:cursor-pointer ">
+                    Visit Site
+                  </h4>
+                </a>
+                <a
+                  href="https://erp-immigration.groupnb.com.ph/ "
+                  target="_blank"
+                >
+                  <FaExternalLinkAlt className="mt-1 hover:scale-125 transition duration-200 hover:cursor-pointer" />
+                </a>
               </div>
             </div>
           </div>
 
           <div className="max-w-full shadow-lg shadow-black-500 h-80 relative bg-black opacity-90 rounded-t-2xl ">
             <img
-              src="./src/img/groupnb.jpg"
+              src={"./static/img/groupnb.JPG"}
               className="h-full w-full opacity-30"
             ></img>
             <div className="flex flex-col gap-4 absolute bottom-0 left-3 px-2 pb-4 z-50 text-white ">
@@ -38,14 +49,27 @@ function Projects() {
                 <p>A company website built with TailwindCSS and ReactJS.</p>
               </div>
               <div className="flex gap-3">
-                <a href="https://erp-immigration.groupnb.com.ph/ " target="_blank"><h4 className="hover:underline hover:cursor-pointer ">Visit Site</h4></a>
-                <a href="https://erp-immigration.groupnb.com.ph/ " target="_blank"><FaExternalLinkAlt className="mt-1 hover:scale-125 transition duration-200 hover:cursor-pointer" /></a>
+                <a
+                  href="https://erp-immigration.groupnb.com.ph/ "
+                  target="_blank"
+                >
+                  <h4 className="hover:underline hover:cursor-pointer ">
+                    Visit Site
+                  </h4>
+                </a>
+                <a
+                  href="https://erp-immigration.groupnb.com.ph/ "
+                  target="_blank"
+                >
+                  <FaExternalLinkAlt className="mt-1 hover:scale-125 transition duration-200 hover:cursor-pointer" />
+                </a>
               </div>
             </div>
           </div>
           <div className="max-w-full shadow-lg shadow-black-500 h-80 relative bg-black opacity-90 rounded-t-2xl ">
             <img
-              src="./src/img/groupnb.jpg"
+              src={"./static/img/groupnb.JPG"}
+              alt=""
               className="h-full w-full opacity-30"
             ></img>
             <div className="flex flex-col gap-4 absolute bottom-0 left-3 px-2 pb-4 z-50 text-white ">
@@ -54,8 +78,20 @@ function Projects() {
                 <p>A company website built with TailwindCSS and ReactJS.</p>
               </div>
               <div className="flex gap-3">
-                <a href="https://erp-immigration.groupnb.com.ph/ " target="_blank"><h4 className="hover:underline hover:cursor-pointer ">Visit Site</h4></a>
-                <a href="https://erp-immigration.groupnb.com.ph/ " target="_blank"><FaExternalLinkAlt className="mt-1 hover:scale-125 transition duration-200 hover:cursor-pointer" /></a>
+                <a
+                  href="https://erp-immigration.groupnb.com.ph/ "
+                  target="_blank"
+                >
+                  <h4 className="hover:underline hover:cursor-pointer ">
+                    Visit Site
+                  </h4>
+                </a>
+                <a
+                  href="https://erp-immigration.groupnb.com.ph/ "
+                  target="_blank"
+                >
+                  <FaExternalLinkAlt className="mt-1 hover:scale-125 transition duration-200 hover:cursor-pointer" />
+                </a>
               </div>
             </div>
           </div>
